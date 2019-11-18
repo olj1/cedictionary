2f9df9a7d91312fa34a281010bfe88f572d20b65
olj1 <john.losier1@gmail.com>
2019-11-18 17:50:54 -0600

    cut queries in half

--- cedictapp/views.py
+++ cedictapp/views.py
@@ -5,5 +5,5 @@ from .serializers import EntriesSerializer
 from rest_framework import viewsets
 
 class EntriesViewSet(viewsets.ModelViewSet):
-    queryset = Entries.objects.all()
+    queryset = Entries.objects.all()[:60000]
     serializer_class = EntriesSerializer
