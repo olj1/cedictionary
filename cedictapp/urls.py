b832a514470c793ae97765cab3c363910ee7fbab
olj1 <john.losier1@gmail.com>
2019-11-18 20:13:59 -0600

    boo

--- cedictapp/urls.py
+++ cedictapp/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include, patterns
+from django.urls import path, include
 from .views import FilteredViewSet # This library gives us all of the functions usually found in views.py
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'', FilteredViewSet, basename='pinyin')
-urlpatterns = patterns('', url(r'^', include(router.urls)))
+urlpatterns = [url(r'^', include(router.urls))]
