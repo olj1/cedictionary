cacc417cc0423579f1423c6a7b555c940ad27477
olj1 <john.losier1@gmail.com>
2019-11-18 19:02:45 -0600

    regex change at urls

--- cedictapp/urls.py
+++ cedictapp/urls.py
@@ -3,5 +3,5 @@ from .views import FilteredViewSet # This library gives us all of the functions 
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'<pinyin>', FilteredViewSet, basename='pinyin')
+router.register(r'pinyin', FilteredViewSet, basename='pinyin')
 urlpatterns = router.urls
